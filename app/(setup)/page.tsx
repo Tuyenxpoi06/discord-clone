0a6a09b929fe777ec08562559b77e26b894308ac
TuyenXipoi <tuyendangvan1411@gmail.com>
2025-07-03 16:14:30 +0700

    initial modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,9 @@
 import { redirect } from "next/navigation";
+
 import { db } from "@/lib/db";
 import { initiaProfile } from "@/lib/initial-profile";
+
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initiaProfile();
@@ -19,7 +22,7 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
